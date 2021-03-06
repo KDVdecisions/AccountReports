fed8c158ba8acac7b840eb8a51dd4b235f49ec17
ashtondrew <ashton.drew@gmail.com>
2021-03-06 11:09:29 -0500

    updated income function to filter by date; inserted Other Income table

--- AccountReportApp/R/ar_paidInvoices.R
+++ AccountReportApp/R/ar_paidInvoices.R
@@ -1,8 +1,8 @@
 # From full income dataframe to a PAID invoices dataframe
 
-ar_paidInvoices <- function(incomeDf){
+ar_paidInvoices <- function(incomeDf, minDate, maxDate){
 	incomeDf %>%
-		dplyr::filter(!is.na(InvNum) & !is.na(PayDate)) %>%
+		dplyr::filter(!is.na(InvNum) & !is.na(PayDate) & as.Date(PayDate) >= minDate & as.Date(PayDate) <= maxDate) %>%
 		dplyr::select(InvNum, Client, InvAmount, InvCurrency, InvDate, PayDate, PayLocate) %>%
 		dplyr::arrange(InvDate)
 }
--- AccountReportApp/R/ar_pendInvoices.R
+++ AccountReportApp/R/ar_pendInvoices.R
@@ -1,8 +1,8 @@
 # From full income dataframe to a PENDING invoices dataframe
 
-ar_pendInvoices <- function(incomeDf){
+ar_pendInvoices <- function(incomeDf, minDate, maxDate){
 	incomeDf %>%
-						dplyr::filter(!is.na(InvNum) & is.na(PayDate)) %>%
+						dplyr::filter(!is.na(InvNum) & is.na(PayDate) & as.Date(InvDate) >= minDate & as.Date(InvDate) <= maxDate) %>%
 						dplyr::select(InvNum, Client, InvAmount, InvCurrency, InvDate) %>%
 						dplyr::arrange(InvDate)
 }
